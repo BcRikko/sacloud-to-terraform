2083b9ff522018bd88eae9918af3d6abda1a349a
BcRikko <s.shin1106@gmail.com>
2017-06-13 14:18:37 +0900

    :+1: APIキーの切り替えできるよう修正

--- README.md
+++ README.md
@@ -29,7 +29,11 @@ $ npm run build
 1. さくらのクラウドで閲覧権限以上のAPIキーをつくる
 2. `env.sample.yml`を`env.yml`にリネームする
 3. `env.yml`にACCESS TOKEN、ACCESS TOKEN SECRET、デフォルトゾーン（`tk1v`など）を設定する
+  * 複数設定可能
+  * 実行時に環境変数ACCOUNTを設定することで、APIキーの切り替えが可能。環境変数ACCOUNTが設定されていない場合は、最初にかかれているアカウントを使用する。
 
 ```
-$ npm run start > sacloud.tf.json
+$ node lib/app.js > sacloud.tf.json
+# OR
+$ ACCOUNT=your-account node lib/app.js > sacloud.tf.json
 ```
--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,18 @@ import * as fs from 'fs';
 import * as yaml from 'js-yaml';
 import Terraform from './Terraform';
 
+interface IEnv {
+    account: string
+    accessToken: string;
+    secretToken: string;
+    defaultZone: string;
+}
+
 process.on('unhandledRejection', console.dir);
 
 async function main() {
-    const config = yaml.safeLoad(fs.readFileSync('env.yml', 'utf-8'));
+    const envs:IEnv[] = yaml.safeLoad(fs.readFileSync('env.yml', 'utf-8'));
+    const config = envs.find(a => a.account === process.env.ACCOUNT) || envs[0]
 
     const tf = new Terraform({
         accessToken: config.accessToken,
